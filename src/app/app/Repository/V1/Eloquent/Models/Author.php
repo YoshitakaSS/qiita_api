dc0dc2b993e480917d69052a47da560da5ce416e
ys <yoshi.23unog34@gmail.com>
2020-05-05 20:32:38 +0900

    @add: Author Model

--- src/app/app/Repository/V1/Eloquent/Models/Author.php
+++ src/app/app/Repository/V1/Eloquent/Models/Author.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Author extends Model
 {
-    //
+    const CREATED_AT = 'create_dt';
+    const UPDATED_AT = 'update_dt';
+
+    // protected $connection = 'mysql';
+    // protected $table = 'qiita_api.authors';
 }
