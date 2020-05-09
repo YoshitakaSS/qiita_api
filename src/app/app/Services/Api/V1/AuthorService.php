9e39125677f8fd01c4863bf87c58f6f149ba7d4b
ys <yoshi.23unog34@gmail.com>
2020-05-09 17:07:20 +0900

    @update: add methods in AuthorService

--- src/app/app/Services/Api/V1/AuthorService.php
+++ src/app/app/Services/Api/V1/AuthorService.php
@@ -1,7 +1,45 @@
 <?php
-namespace App\Services\V1;
+namespace App\Services\Api\V1;
+
+use App\Repositories\V1\Interfaces\AuthorInterface;
+use App\Http\Resources\InfoResource;
+use App\Http\Resources\AuthorsResource;
 
 class AuthorService
 {
+    private $authorRepository;
+    protected $infoResource;
+    protected $authorResource;
 
+    public function __construct(AuthorInterface $authorRepository)
+    {
+        $this->authorRepository = $authorRepository;
+        $this->infoResource = new InfoResource(
+            'Author List API',
+            'Returns a list of authors trending in Qiita'
+        );
+
+    }
+
+    public function getAllList()
+    {
+        $authorsList = $this->authorRepository->getAllList();
+
+        $authorResource = new AuthorsResource($authorsList);
+
+        return [
+            'Info' => $this->infoResource,
+            'List' => $authorResource
+        ];
+    }
+
+    public function getListByAuthorName($name)
+    {
+        $this->authorRepository->getListByAuthorName($name);
+    }
+
+    public function getListByCount($count)
+    {
+        $this->authorRepository->getListByCount($count);
+    }
 }
