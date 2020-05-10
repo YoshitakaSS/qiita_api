2994a6dffe8eab9f40f7ae9fe88c35c8581372fa
ys <yoshi.23unog34@gmail.com>
2020-05-10 22:01:09 +0900

    @add: migration for authors

---
+++ src/app/database/migrations/2020_05_04_023001_create_authors_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAuthorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('authors', function (Blueprint $table) {
+            $table->increments('author_id');
+            $table->string('author_name')->unique();
+            $table->integer('count');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('authors');
+    }
+}
