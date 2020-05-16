84ff8824e72feb7955164b393ebb5172c23cae5a
ys <yoshi.23unog34@gmail.com>
2020-05-16 20:37:43 +0900

    @add: job repository

---
+++ src/app/app/Repositories/V1/Eloquent/JobRepository.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Repositories\V1\Eloquent;
+
+use App\Repositories\V1\Eloquent\Models\Job;
+use App\Repositories\V1\Interfaces\JobInterface;
+
+class JobRepository implements JobInterface
+{
+    private $job;
+
+    public function __construct(Job $job)
+    {
+        $this->job = $job;
+    }
+
+    public function getJobData($jobName)
+    {
+        return $this->job
+                    ->where('job_name', $jobName)
+                    ->first();
+
+    }
+
+    /**
+     * jobを保存する
+     * job_status: default 100（未実行
+     *                     200（実行中
+     *                     300（エラー
+     *
+     * @param string
+     * @param string
+     */
+    public function storeJobData($jobName, $jobDescription)
+    {
+        return $this->job
+                    ->insert([
+                        'job_name'          => $jobName,
+                        'job_description'   => $jobDescription,
+                        'job_status'        => 100,
+                        'created_at'        => now(),
+                    ]);
+    }
+
+    /**
+     * @param string
+     * @param int
+     * @param bool
+     */
+    public function updateJobStatus($jobName, $status, $isRun)
+    {
+        $updateList = [
+            'job_status' => $status
+        ];
+
+        $dateTime = date('Y-m-d H:m:s');
+        $jobDate = $isRun ? ['job_last_start_time' => $dateTime] : ['job_last_end_time' => $dateTime];
+
+        $updateList = array_merge($updateList, $jobDate);
+
+        return $this->job
+                    ->where('job_name', $jobName)
+                    ->update($updateList);
+    }
+}
