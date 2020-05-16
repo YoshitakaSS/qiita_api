71617ae379276f1a8d912d993da6d5c13d3898f0
ys <yoshi.23unog34@gmail.com>
2020-05-16 20:37:55 +0900

    @add: job service

---
+++ src/app/app/Services/JobService.php
@@ -0,0 +1,43 @@
+<?php
+namespace App\Services;
+
+use App\Repositories\V1\Interfaces\JobInterface;
+
+class JobService
+{
+    private $jobRepository;
+    private $jobName;
+
+    public function __construct(JobInterface $jobRepository, $jobName)
+    {
+        $this->jobRepository = $jobRepository;
+        $this->jobName = $jobName;
+    }
+
+    /**
+     * ジョブを取得する
+     *
+     * @return array
+     */
+    public function getJobData()
+    {
+        // jobの確認を行う
+        return $this->jobRepository->getJobData($this->jobName);
+    }
+
+    /**
+     * jobを止める
+     */
+    public function stopJob()
+    {
+        return $this->jobRepository->updateJobStatus($this->jobName, 100, false);
+    }
+
+    /**
+     * jobを実行する
+     */
+    public function runJob()
+    {
+       return $this->jobRepository->updateJobStatus($this->jobName, 200, true);
+    }
+}
