82a0ef64a7d880f197f20cf60bf1eb1f014a6137
ys <yoshi.23unog34@gmail.com>
2020-05-20 19:55:54 +0900

    @add: tags resource

---
+++ src/app/app/Http/Resources/TagsResource.php
@@ -0,0 +1,34 @@
+<?php
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class TagsResource extends ResourceCollection
+{
+    public function __construct($resource)
+    {
+        $this->resources = $resource;
+    }
+
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        $list = [];
+        foreach ($this->resources as $resource) {
+            $list[] = [
+                'tagId'          => $resource->tag_id,
+                'tagName'        => $resource->tag_name,
+                'tagUrl'         => $resource->tag_url,
+                'tagCount'       => $resource->tag_count,
+                'createDate'     => $resource->created_at,
+                'updateDate'     => $resource->updated_at,
+            ];
+        }
+        return $list;
+    }
+}
