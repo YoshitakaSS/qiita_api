1777954f90330b1747a409bfe6914349774ce00b
ys <yoshi.23unog34@gmail.com>
2020-05-20 19:54:37 +0900

    @add: job seeder

--- src/app/database/seeds/JobsSeeder.php
+++ src/app/database/seeds/JobsSeeder.php
@@ -12,14 +12,29 @@ class JobsSeeder extends Seeder
      */
     public function run()
     {
+        // データのクリア
+        DB::table('authors')->truncate();
+
         $paramList = [
             [
-                'insert_author_job',
-                'Qiitaのトレンド入りした著者を格納する',
-                100,
-                0,
-                '2000-01-01 00:00:00',
-                '2000-01-01 00:00:00'
+                'job_name'                  => 'insert_author_job',
+                'job_description'           => 'Qiitaのトレンド入りした著者を格納する',
+                'job_status'                => 100,
+                'is_last_succeeded'         => 0,
+                'job_last_start_time'       => now(),
+                'job_last_end_time'         => now(),
+                'created_at'                => '2000-01-01 00:00:00',
+                'updated_at'                => '2000-01-01 00:00:00'
+            ],
+            [
+                'job_name'                  => 'insert_tags_job',
+                'job_description'           => 'Qiitaのトレンド入りしたタグを格納する',
+                'job_status'                => 100,
+                'is_last_succeeded'         => 0,
+                'job_last_start_time'       => now(),
+                'job_last_end_time'         => now(),
+                'created_at'                => '2000-01-01 00:00:00',
+                'updated_at'                => '2000-01-01 00:00:00'
             ]
         ];
 
