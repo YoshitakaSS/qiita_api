09451caee63d26b1bb5591229b5897d5d483c238
ys <yoshi.23unog34@gmail.com>
2020-06-07 15:27:45 +0900

    @add: ListContorller

---
+++ src/app/app/Http/Controllers/Api/V1/ListController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Services\Api\V1\ListService;
+use Illuminate\Http\Request;
+
+class ListController extends Controller
+{
+    public function index()
+    {
+
+    }
+}
