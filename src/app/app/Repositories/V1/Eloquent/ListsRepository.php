97b297f6c6d192a7ec0a4ba770880ce524a67c64
ys <yoshi.23unog34@gmail.com>
2020-06-14 12:00:14 +0900

    @add: ListRepository

---
+++ src/app/app/Repositories/V1/Eloquent/ListsRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Repositories\V1\Eloquent;
+
+use App\Repositories\V1\Eloquent\Models\Lists;
+use App\Repositories\V1\Interfaces\ListInterface;
+
+class ListRepository implements ListInterface
+{
+}
