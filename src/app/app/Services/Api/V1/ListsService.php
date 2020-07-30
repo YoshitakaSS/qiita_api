2808538b7d0cc1f9b79fbe75928cb43c036c9b7d
ys <yoshi.23unog34@gmail.com>
2020-07-30 22:29:48 +0900

    @update: ListService

--- src/app/app/Services/Api/V1/ListsService.php
+++ src/app/app/Services/Api/V1/ListsService.php
@@ -2,8 +2,25 @@
 
 namespace App\Services\Api\V1;
 
+use App\Repositories\V1\Interfaces\ListsInterface;
+use App\Http\Resources\ListsReosource;
+use App\Http\Resources\InfoResource;
+
 class ListsService
 {
+    protected $infoResource;
+    protected $listsRepository;
+
+    public function __construct(ListsReosource $listsRepository)
+    {
+        $this->listsRepository = $listsRepository;
+        $this->infoResource = new InfoResource(
+            'Article List API',
+            'Returns a list of article trending in Qiita'
+        );
+
+    }
+
     public function getAllList()
     {
 
