82c69945e4ac5bd1ee44bd9440cf805b55f8c7d4
ys <yoshi.23unog34@gmail.com>
2020-08-22 18:58:14 +0900

    @add: AuthorUnitTest

---
+++ src/app/tests/Unit/AuthorTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Repositories\V1\Eloquent\Models\Author;
+use App\User;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use PHPUnit\Framework\TestCase;
+
+class AuthorTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private $author;
+
+    public function setUp():void
+    {
+        parent::setUp();
+    }
+
+    public function testAuthorCommand()
+    {
+        // Artisan::call('insertAuthors');
+        $today = date('Y-m-d');
+        $authorJson = file_get_contents("/mnt/json/author/$today.json");
+        $authorList = json_decode($authorJson, true);
+
+        // Modelを使用してデータが入ったのか確認する
+    }
+}
