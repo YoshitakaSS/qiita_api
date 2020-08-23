c7d603c6d4da2f1db90123f99ff97990b35de5e6
ys <yoshi.23unog34@gmail.com>
2020-08-23 20:00:55 +0900

    @fix: change file name

---
+++ src/app/tests/Unit/AuthorCommandTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Repositories\V1\Eloquent\Models\Author;
+use App\User;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class AuthorCommandTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private $author;
+
+    public function setUp():void
+    {
+        parent::setUp();
+        // データをセット
+        // $this->setData();
+    }
+
+    public function testAuthorCommand()
+    {
+        Artisan::call('insertAuthors');
+        $this->author = new Author();
+        dd($this->author->get());
+
+        $authorJson = file_get_contents(__DIR__ . '/Resources/AuthorUnitResource.json');
+        $authorList = json_decode($authorJson, true);
+
+        // Modelを使用してデータが入ったのか確認する
+        // $this->author = new Author();
+        $this->author = factory(Author::class)->make();
+    }
+
+    protected function setData()
+    {
+        factory(Author::class)->create([
+            'author_id' => 121,
+            'author_name' => 'hoge',
+            'count' => 1,
+            'created_at' => '2020-10-10 21:00:00',
+            'updated_at' => '2020-10-10 22:00:00',
+        ]);
+        factory(Author::class)->create([
+            'author_id' => 140,
+            'author_name' => 'fuga',
+            'count' => 1,
+            'created_at' => '2020-11-11 21:00:00',
+            'updated_at' => '2020-12-10 22:00:00',
+        ]);
+        factory(Author::class)->create([
+            'author_id' => 124,
+            'author_name' => 'saga',
+            'count' => 1,
+            'created_at' => '2020-12-23 21:00:00',
+            'updated_at' => '2020-12-25 22:30:00',
+        ]);
+        factory(Author::class)->create([
+            'author_id' => 124,
+            'author_name' => 'test-user',
+            'count' => 1,
+            'created_at' => '2020-12-24 21:00:00',
+            'updated_at' => '2020-12-25 22:31:00',
+        ]);
+    }
+}
