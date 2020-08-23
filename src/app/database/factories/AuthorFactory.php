0eed16ecc50ff74075ee8b6d1c66fdf40d0661b9
ys <yoshi.23unog34@gmail.com>
2020-08-23 20:00:14 +0900

    add: AuthorFactory

---
+++ src/app/database/factories/AuthorFactory.php
@@ -0,0 +1,10 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Repositories\V1\Eloquent\Models\Author;
+use Faker\Generator as Faker;
+
+$factory->define(Author::class, function (Faker $faker) {
+    return [];
+});
